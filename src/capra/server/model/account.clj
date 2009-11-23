e93a26509c54a39cd9f68ee6fcd794bcac599cbb
weavejester <jreeves@weavejester.com>
2009-11-23 01:54:14 +0000

    Account passwords stored bcrypted

--- src/capra/server/model/account.clj
+++ src/capra/server/model/account.clj
@@ -2,17 +2,25 @@
   "Functions for accessing the account model."
   (:refer-clojure :exclude [get])
   (:use capra.server.util)
-  (:use somnium.congomongo))
+  (:use somnium.congomongo)
+  (:import BCrypt))
 
 (defn get
   "Retrieve an existing account by name."
   [name]
   (fetch-one :accounts :where {:name name}))
 
+(defn- hash-passkey
+  "Ensure the passkey is hashed before saving."
+  [account]
+  (update-in account [:passkey]
+    (fn [passkey]
+      (BCrypt/hashpw passkey (BCrypt/gensalt)))))
+
 (defn put
   "Save a new account using the name as a key."
   [account]
-  (insert! :accounts account))
+  (insert! :accounts (hash-passkey account)))
 
 (defn list-names
   "Retrieve all current account names."
@@ -23,4 +31,4 @@
   "Is the supplied account name and passkey valid?"
   [name pass]
   (if (and name pass)
-    (= (:passkey (get name)) pass)))
+    (BCrypt/checkpw pass (:passkey (get name)))))
